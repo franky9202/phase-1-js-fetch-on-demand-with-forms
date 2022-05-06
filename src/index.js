7aaa5bfa0479def55dafed7fc5ff1a983efc2022
Edgar <franky9202@gmail.com>
2022-05-06 11:15:56 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form').addEventListener
+    ("submit",function (e){
+        e.preventDefault()
+        const input = e.target.searchByID.value
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
